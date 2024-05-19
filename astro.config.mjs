bfdf7ab14536a22a18eda35f2deebc5102ee179d
patricioreguera <reguerapatriciohernan@gmail.com>
2024-05-19 21:41:36 +0200

    test configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
-/* import auth from "auth-astro"; */
+import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), db() /* , auth() */],
+	integrations: [tailwind(), db(), auth()],
 	output: "server",
 	adapter: vercel(),
 });
