a31d6cdd33c36b0b1f1dcc7306a128a06dba91dd
patricioreguera <reguerapatriciohernan@gmail.com>
2024-05-19 21:09:04 +0200

    Feat: add auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 import vercel from "@astrojs/vercel/serverless";
-
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
+import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), db()],
+	integrations: [tailwind(), db(), auth()],
 	output: "server",
 	adapter: vercel(),
 });
+
+//npx astro add db
+//npx astro login
+//npx astro link
+//npx astro add auth-astro para autorizaciones
+//  npm run astro db push --remote  para pushear la db local
+// npm install @astrojs/vercel  para instalar vercel
+//npm install @astrojs/tailwind tailwindcss taiwind install
